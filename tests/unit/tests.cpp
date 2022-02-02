6c0c0fc94511ae73a7358c0992b3f80827afa69a
Andrey Berdnik <andrey.berdnik@gmail.com>
2022-02-02 20:28:55 +0200

    Fix codestyle in tests

--- tests/unit/tests.cpp
+++ tests/unit/tests.cpp
@@ -1,8 +1,7 @@
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
-int main(int argc, char *argv[])
-{
-    ::testing::InitGoogleMock(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
 }
