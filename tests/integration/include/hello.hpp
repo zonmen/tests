a1e7a615de5d8cc7e44d9c7726946ac8900fc58f
Storegist <92214365+Storegist@users.noreply.github.com>
2022-02-04 16:56:25 +0200

    Delete hello.hpp

--- tests/integration/include/hello.hpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-void print_hello (void); 
-
-void print_arg(const std::string& str);
-
-void print_arg2(const std::string& str1, const std::string& str2);
-
-void print_arg3(const std::string& str3);
